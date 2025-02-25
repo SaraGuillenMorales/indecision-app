cae54cc23bf49781c439f245796f2d91bb256fa4
Sara Guillen <saraguillencole@gmail.com>
2025-02-25 20:18:55 +0100

    Componentes del chat

---
+++ src/components/chat/chatNuntii.vue
@@ -0,0 +1,19 @@
+<template>
+
+<div class="p-4 flex-1 overflow-y-auto">
+    <div class="flex flex-col space-y-2">
+    <div class="flex justify-end">
+        <div class="bg-blue-200 text-black p-2 m-w-xs rounded-b-lg">
+         Hola, cómo te va el dia?
+        </div>
+    </div>
+
+<div class="flex">
+    <div class="bg-gray-300 text-black p-2 m-w-xs rounded-b-lg">
+        Muy bien, ahora estoy en casa. Y tú?
+    </div>
+</div>
+</div>
+</div>
+
+</template>
