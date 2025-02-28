4cc84f5f4fb919a84b8b628932e7e845f7c82c2f
Sara Guillen <saraguillencole@gmail.com>
2025-02-28 10:59:43 +0100

    Chat con gif de YesNo

--- src/components/chat/chatNuntii.vue
+++ src/components/chat/chatNuntii.vue
@@ -3,8 +3,11 @@
 <div class="p-4 flex-1 overflow-y-auto">
     <div class="flex flex-col space-y-2">
 
-            <ChatBullula: meus-est="true" nuntius=""Salimos luego a dar una vuelta? />
-
+            <ChatBullula: meus-est="true" nuntius="Salimos luego a dar una vuelta?" />
+            <ChatBullula: 
+            meus-est="false" 
+            nuntius="no" 
+            image="https://yesno.wtf/assets/yes/2.gif"/>
 </div>
 </div>
 
